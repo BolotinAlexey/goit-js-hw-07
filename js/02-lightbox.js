5ecc828aec6d47c43c25be44d043b56939e3f413
Oleksii Bolotin <samkiral73@gmail.com>
2023-01-13 23:13:17 +0100

    finish task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+createGalleryInDOM();
+
+// config gallery
+const gallerySetting = {
+  captionDelay: 250,
+  captionsData: 'alt',
+};
+
+// on turn library "SimpleLightbox"
+const lightbox = new SimpleLightbox('.gallery a', gallerySetting);
+
+// create HTML markdown of gallery
+function createGalleryInDOM() {
+  const injectHTML = galleryItems.map(createGalleryItem).join('');
+  galleryEl.insertAdjacentHTML('beforeend', injectHTML);
+}
+
+// create a element of gallery
+function createGalleryItem(el) {
+  return `<a class="gallery__item" href= ${el.original}>
+  <img class="gallery__image" src=${el.preview} alt=${el.description} />
+</a>`;
+}
